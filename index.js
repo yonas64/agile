8640953e513e8ce998c8c7c446f0884d2c8a6ab9
yonna <yonasbezawork@gmail.com>
2026-04-07 10:48:46 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express'); // Import Express
+const app = express();               // Create an Express application
+const port = 3000;                   // Define the port to listen on
+
+// Define a route for the root URL ("/")
+app.get('/', (req, res) => {
+  res.send('Hello World!');           // Send the response "Hello World!"
+});
+
+// Start the server and listen for connections
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
